e6ff96e50067bfb9216cea0bc192ffb02207d4bf
Billy Bright <billybright1992@gmail.com>
2019-08-18 21:30:09 -0700

    axios working and recived jasonData

--- assets/lilr.js
+++ assets/lilr.js
@@ -1,10 +1,10 @@
 var axios = require("axios");
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+//var keys = require("./keys.js");
+//var spotify = new Spotify(keys.spotify);
 
-var query = process.argv[2];
+var lookUpCode = process.argv[2];
 
-if ((query = "spotify-this-song")) {
+/*if ((lookUpCode = "spotify-this-song")) {
   var search = process.argv.slice(3).join(" ");
 
   spotify
@@ -15,23 +15,19 @@ if ((query = "spotify-this-song")) {
     .catch(function(err) {
       console.log(err);
     });
-} else if ((query = "moivie-this")) {
+} else */ if (
+  (lookUpCode = "moivie-this")
+) {
   var movieName = process.argv.slice(3).join(" ");
 
-  // Then run a request to the OMDB API with the movie specified
   var queryUrl =
     "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-  // This line is just to help us debug against the actual URL.
-  console.log(queryUrl);
-
-  axios(queryUrl, function(error, response, body) {
-    // If the request is successful
-    if (!error && response.statusCode === 200) {
-      // Parse the body of the site and recover just the imdbRating
-      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-      console.log("Release Year: " + JSON.parse(body).Year);
-    }
+  axios.get(queryUrl).then(function(response) {
+    var jsonData = response.data;
+    console.log(jsonData);
+    if (err) throw err;
+    console.log(showData);
   });
-} else if ((query = "concert-this")) {
+} else if ((lookUpCode = "concert-this")) {
 }

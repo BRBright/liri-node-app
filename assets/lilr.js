c91cb7151907d3a455fd6995a13b34d5de2de27f
Billy Bright <billybright1992@gmail.com>
2019-08-20 22:10:42 -0700

    get user input by asking the user in terminal and get what they want to search.

--- assets/lilr.js
+++ assets/lilr.js
@@ -1,45 +1,66 @@
 var axios = require("axios");
+var inquirer = require("inquirer");
 var keys = require("./keys.js");
 const Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
-var lookUpCode = process.argv[2];
-
-if ((lookUpCode = "spotify-this-song")) {
-  var search = process.argv.slice(3).join(" ");
-
-  spotify.search({ type: "track", query: search }, function(err, data) {
-    if (err) {
-      return console.log("Error occurred: " + err);
+inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "What would you like to search for?",
+      choices: [
+        "Search Spotify for a song.",
+        "Look for a conert by artist.",
+        "Search for a movie."
+      ],
+      name: "searchChoice"
+    },
+    {
+      type: "input",
+      message: "Search",
+      name: "search"
     }
-    console.log(data);
+  ])
+  .then(function(inquirerResponse) {
+    if (inquirerResponse.searchChoice === "Search Spotify for a song.") {
+      var search = inquirerResponse.search;
+
+      spotify.search({ type: "track", query: search }, function(err, data) {
+        if (err) {
+          return console.log("Error occurred: " + err);
+        }
+        console.log(data);
+      });
+    } else if (inquirerResponse.searchChoice === "Search for a movie.") {
+      var movieName = inquirerResponse.search;
+
+      var queryUrl =
+        "http://www.omdbapi.com/?t=" +
+        movieName +
+        "&y=&plot=short&apikey=trilogy";
+
+      axios.get(queryUrl).then(function(response) {
+        var jsonData = response.data;
+        console.log(jsonData);
+        if (err) throw err;
+        console.log(showData);
+      });
+    } else if (
+      inquirerResponse.searchChoice === "Look for a conert by artist."
+    ) {
+      var artist = inquirerResponse.search;
+
+      var queryUrl =
+        "https://rest.bandsintown.com/artists/" +
+        artist +
+        "/events?app_id=codingbootcamp";
+
+      axios.get(queryUrl).then(function(response) {
+        var jsonData = response.data;
+        console.log(jsonData);
+        if (err) throw err;
+        console.log(showData);
+      });
+    }
   });
-} else if ((lookUpCode = "moivie-this")) {
-  var movieName = process.argv.slice(3).join(" ");
-
-  var queryUrl =
-    "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-  axios.get(queryUrl).then(function(response) {
-    var jsonData = response.data;
-    console.log(jsonData);
-    if (err) throw err;
-    console.log(showData);
-  });
-} else if ((lookUpCode = "concert-this")) {
-  var artist = process.argv.slice(3).join(" ");
-
-  var queryUrl =
-    "https://rest.bandsintown.com/artists/" +
-    artist +
-    "/events?app_id=codingbootcamp";
-
-  axios.get(queryUrl).then(function(response) {
-    var jsonData = response.data;
-    console.log(jsonData);
-    if (err) throw err;
-    console.log(showData);
-  });
-} else {
-  console.log("Spelling.");
-}
